dcc39ab066f4b7cf154c7c38736070867c0a01f3
Raphael Javaux <raphaeljavaux@gmail.com>
2015-09-10 00:06:03 +0200

    FlickrPushToAPI script

--- FlickrPushToAPI.hs
+++ FlickrPushToAPI.hs
@@ -1,27 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Applicative
-import Control.Monad
-import Control.Monad.Reader (runReaderT)
+import Control.Monad (mzero)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger (LoggingT, runStderrLoggingT)
+import Control.Monad.Reader (runReaderT)
+import Data.Monoid
+import Data.Aeson (FromJSON (..),  Value (Object), (.:), (.:?), decode')
 import Data.ByteString (ByteString)
 import Data.String (fromString)
+import Data.Text (Text)
+import Data.Foldable (forM_)
 import Database.Persist (
-      Entity (..), (==.), selectKeysList, selectList, insert_
+      Entity (..), selectList, insert_, insertBy
     )
-import Database.Persist.Sql (runMigration)
+import Database.Persist.Sql (runMigration, transactionSave)
 import Database.Persist.Sqlite (SqlPersistT, runSqlConn, withSqliteConn)
 import Network.HTTP.Client (Manager, managerResponseTimeout, withManager)
 import Network.HTTP.Client.Conduit (HasHttpManager (getHttpManager))
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import System.Directory (getDirectoryContents)
 import System.Environment (getArgs)
-import System.FilePath ((</>), takeBaseName)
+import System.FilePath ((</>), (<.>), takeBaseName)
+import Text.Printf (printf)
 
-import qualified Data.Map   as M
-import qualified Data.Text  as T
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.Set                   as S
+import qualified Data.Text                  as T
 
-import API (HasAPIConfig (..), postImage)
+import API (HasAPIConfig (..), postObjects)
 import Model
 
 data APIEnvironment = APIEnvironment {
@@ -37,53 +44,99 @@ instance HasAPIConfig APIEnvironment where
     getApiRoot = aeApiRoot
     getApiKey  = aeApiKey
 
+data Picture = Picture {
+      pId       :: Text
+    , pTitle    :: Text
+    , pOwner    :: Owner
+    , pUrl      :: Text
+    , pTags     :: [Text]
+    } deriving Show
+
+instance FromJSON Picture where
+    parseJSON (Object o) = Picture <$> o .: "id"
+                                   <*> o .: "title"
+                                   <*> o .: "owner"
+                                   <*> o .: "url"
+                                   <*> o .: "tags"
+    parseJSON _          = mzero
+
+data Owner = Owner {
+      oId       :: Text
+    , oName     :: Maybe Text
+    , oUsername :: Maybe Text
+    } deriving Show
+
+instance FromJSON Owner where
+    parseJSON (Object o) = Owner <$> o .: "id"
+                                 <*> o .:? "name"
+                                 <*> o .:? "username"
+    parseJSON _          = mzero
+
 -- | Pushes every local image into the API end-point.
 --
--- usage: flickr-push-to-api <api root> <api key> <sqlite db> <dir> <tag>
--- where <api root> is the URL of end-point of the API, <api key> the private
--- API key, <sqlite db> the database which contains the image mapping, <dir> the
--- directory which contains the images and <tag> the tag which will be used to
--- group the images and to prefix their tags.
+-- usage: flickr-push-to-api <api root> <api key>  sqlite db> <flickr dir> <tag>
+-- where <api root> is the URL of end point of the API, <api key> the private
+-- API key, <sqlite db> the database which will contains image meta-data, <dir>
+-- the directory which contains the crawled images and <tag> the tag prefix 
+-- which will be used to group the images and their tags.
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [apiRoot, apiKey, sqliteFile, dir, tag] -> do
+        [apiRoot, apiKey, sqliteFile, dir, mainTag] -> do
+            let tagPrefix = T.pack mainTag <> ":"
+
             -- Creates a mapping between photo id and the corresponding
-            -- filepath.
+            -- filepaths (.jpg image et .json meta-data files).
             fs <- getDirectoryContents dir
-            let photoIdToFile = M.fromList [ (T.pack filename, dir </> f)
-                                           | f <- fs
-                                           , let filename = takeBaseName f ]
+            let localIds = S.fromList [ T.pack $ takeBaseName f
+                                      | f <-  fs
+                                      , head f /= '.' ]
 
             withHttpSqlite sqliteFile $ \manager -> do
                 runMigration migrateFlickr
 
-                let env       = APIEnvironment manager apiRoot
-                                               (fromString apiKey)
-                    tagTxt    = T.pack tag
-                    tagPrefix = tag ++ ":"
+                apiIds <- (S.fromList . map (flickrPictureFlickrId . entityVal))
+                          <$> selectList [] []
 
-                pics <- selectList [] []
-                forM_ pics $ \(Entity picId pic) -> do
-                    let photoId   = flickrImagePhotoId pic
-                        Just path = photoId `M.lookup` photoIdToFile
+                let notInApisIds = S.difference localIds apiIds
+                liftIO $ printf "%d of %d images not in the API\n"
+                                (S.size notInApisIds) (S.size localIds)
 
-                    tags <-     map (T.pack . (tagPrefix ++) . show)
-                            <$> selectKeysList [ FlickrImageTagImage ==. picId ]
-                                               []
+                let env = APIEnvironment manager apiRoot (fromString apiKey)
 
-                    Just code <- runReaderT (postImage path (tagTxt : tags)) env
+                forM_ notInApisIds $ \flickrId -> do
+                    let basePath = dir </> T.unpack flickrId
+                        jsonPath = basePath <.> "json"
+                        jpgPath  = basePath <.> "jpg"
 
-                    insert_ (ApiImage code picId)
+                    liftIO $ printf "Inserting %s ...\n" jpgPath
+
+                    Just Picture {..} <-
+                        liftIO $! decode' <$> C.readFile jsonPath
+
+                    -- Pushs to the API.
+                    Just apiId <- flip runReaderT env $
+                        postObjects (Just pUrl) jpgPath 
+                                    (map (tagPrefix <>) pTags) False
+
+                    -- Inserts the meta-data into the database.
+                    let Owner {..} = pOwner
+                    eOwner <- insertBy $ FlickrOwner pId oName oUsername
+                    let ownerId = either entityKey id eOwner
+
+                    insert_ $ FlickrPicture pId apiId pTitle ownerId pUrl pTags
+
+                    transactionSave
         _ -> do
             putStrLn "usage: flickr-push-to-api <api root> <api key> \
-                     \<sqlite db> <dir> <tag>"
+                     \<sqlite db> <flickr dir> <tag>"
             putStrLn "where <api root> is the URL of end point of the API,"
-            putStrLn "<api key> the private API key, <sqlite db> the database"
-            putStrLn "which contains the image mapping, <dir> the directory"
-            putStrLn "which contains the images and <tag> the tag which will"
-            putStrLn "be used to group the images and to prefix their tags."
+            putStrLn "<api key> the private API key, <sqlite db> the database "
+            putStrLn "which will contains image meta-datas, <dir> the "
+            putStrLn "directory which contains the crawled images and <tag> "
+            putStrLn "the tag prefix which will be used to group the images "
+            putStrLn "and their tags."
 
 -- | Runs the given action with an HTTP manager and a SQLite connection.
 withHttpSqlite :: String -> (Manager -> SqlPersistT (LoggingT IO) a) -> IO a
--- flickr_crawler.py
+++ flickr_crawler.py
@@ -4,6 +4,7 @@
 import argparse
 import flickrapi
 import json
+import os
 import os.path
 import urllib
 
@@ -17,22 +18,34 @@ SOURCE_SIZE = "Medium 640"
 Photo = namedtuple("Photo", "id title owner tags url")
 Owner = namedtuple("Owner", "id username name")
 
-def photos_stream(flickr):
+def photos_stream(ids, flickr, license):
     """
     Returns an iteraror of Photo objects until the Flickr search is exhausted.
     """
 
     page = 1
+    prev_ids = set()
     while True:
+        print ("Asking for page %d ..." % page)
+
         photos = flickr.photos.search(
-            page=page, sort="relevance", license="4" # CC Attribution License
+            page=page, sort="relevance", license=license # CC Attribution License
         )
         if is_flickr_error(photos):
             print("Unable to fetch the search results. Retrying ...")
             continue
 
+        this_ids = set(p["id"] for p in photos["photos"]["photo"])
+        if this_ids == prev_ids:
+            print("Flickr gave the same results as the previous request. Stop.")
+            return
+        prev_ids = this_ids
+
         for p in photos["photos"]["photo"]:
             # Fetches meta-data and source image for each result.
+            if p["id"] in ids:
+                print("File %s already exists. Skip." % p["id"])
+                continue
 
             info = flickr.photos.getInfo(photo_id=p["id"], secret=p["secret"])
             if is_flickr_error(info):
@@ -53,19 +66,20 @@ def photos_stream(flickr):
             source = find(
                 lambda size: size["label"] == SOURCE_SIZE,
                 sizes["sizes"]["size"]
-            )["source"]
+            )
             if source == None:
                 print(
                     "The picture (%s) has no corresponding image. Skip."
                     % p["id"]
                 )
                 continue
-            if source[-4:] != ".jpg" and source[-5:] != ".jpeg":
+            source_url = source["source"]
+            if source_url[-4:] != ".jpg" and source_url[-5:] != ".jpeg":
                 print("The picture (%s) is not a JPEG image. Skip." % p["id"])
                 continue
 
             try:
-                jpg = download_url(source)
+                jpg = download_url(source_url)
             except:
                 print("Failed to download the picture (%s). Skip." % p["id"])
                 continue
@@ -75,9 +89,9 @@ def photos_stream(flickr):
                 info["photo"]["owner"]["realname"]
             )
 
-            tags = [ t["raw"]
+            tags = [ t["raw"].lower()
                      for t in info["photo"]["tags"]["tag"]
-                     if not t["machine_tag"] ]
+                     if not t["machine_tag"] and is_valid_tag(t["raw"]) ]
 
             url = "https://www.flickr.com/photos/{0}/{1}/".format(
                 owner.id, p["id"]
@@ -85,12 +99,23 @@ def photos_stream(flickr):
 
             yield (Photo(p["id"], p["title"], owner, tags, url), jpg)
 
+            ids.add(p["id"])
+
         page += 1
         if page >= photos["photos"]["pages"]:
             return
 
 def is_flickr_error(resp):
     return resp["stat"] != "ok"
+
+def is_valid_tag(tag):
+    def is_ascii_alpha_num(c):
+        c_ord = ord(c)
+        return ord('A') <= c_ord <= ord('Z') or \
+               ord('a') <= c_ord <= ord('z') or \
+               ord('0') <= c_ord <= ord('9')
+
+    return all(is_ascii_alpha_num(c) for c in tag)
 
 def download_url(url):
     """Returns the file content."""
@@ -132,6 +157,20 @@ def get_cli_args_parser():
         help='Where the fetched images and meta-data should be stored'
     )
 
+    parser.add_argument(
+        "--license", metavar="license", type=int, default=4,
+        help="""License of downloaded pictures (0: All Rights Reserved,
+             1: Attribution-NonCommercial-ShareAlike License,
+             2: Attribution-NonCommercial License,
+             3: Attribution-NonCommercial-NoDerivs License,
+             4: Attribution License,
+             5: Attribution-ShareAlike License,
+             6: Attribution-NoDerivs License,
+             7: No known copyright restrictions,
+             8: United States Government Work)
+             """
+    )
+
     return parser
 
 def find(pred, xs):
@@ -141,15 +180,24 @@ def find(pred, xs):
 
     return None
 
+def existing_files(dest_dir):
+    """Returns the set of IDs of existing files."""
+    return set(os.path.splitext(f)[0] for f in os.listdir(dest_dir)
+               if os.path.isfile(os.path.join(dest_dir, f)))
+
 if __name__ == "__main__":
     cli_args   = get_cli_args_parser().parse_args()
     api_key    = cli_args.api_key
     api_secret = cli_args.api_secret
     dest_dir   = cli_args.dest_dir
+    license    = cli_args.license
 
     flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
 
-    for (p, jpg) in photos_stream(flickr):
+    ids = existing_files(dest_dir)
+    print ("%d pictures already downloaded." % len(ids))
+
+    for (p, jpg) in photos_stream(ids, flickr, license):
         basename  = os.path.join(dest_dir, p.id)
         jpg_file  = basename + ".jpg"
         json_file = basename + ".json"
